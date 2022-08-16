3c7f8afc2b703a5263bdfafadb22f4ead594d56b
Doro256 <dorothynantumbwe44@gmail.com>
2022-08-16 08:31:19 +0000

    Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,89 @@
+#include "sort.h"
+
+/**
+   * quick_sort - Sorts an array of integers in ascending order using the Q\
+   uick
+   * sort algorithm
+   * @array: Pointer to an array
+   * @size: Size of the array
+   * Return: Nothing
+   */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quickSort(array, 0, size);
+}
+
+/**
+  * partition - Partitions a given list of unsorted numbers
+  * @array: Array to be sorted
+  * @left: lower boundary
+  * @right: upper boundary
+  * Return: Nothing
+  */
+
+int partition(int *array, int left, int right)
+{
+	int pivot = array[left];
+	int i = left, j = right;
+
+	do {
+		do {
+			i++;
+		} while (array[i] <= pivot);
+		do {
+			j--;
+		} while (array[j] > pivot);
+
+		if (i < j)
+		{
+			swap(&array[i], &array[j]);
+			/* print_array(array, size); */
+		}
+	} while (i < j);
+
+	swap(&array[left], &array[j]);
+	/* print_array(array, size); */
+	return (j);
+
+}
+
+/**
+  * swap - function to swap two integers
+  * Return: void
+  * @a: integer 1
+  * @b: integer 2
+  */
+
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+  * quickSort - Sorts an array of integers in ascending order using the Quick
+  * sort algorithm
+  * @array: Pointer to an array
+  * @left: Starting index
+  * @right: Ending index
+  * Return: Nothing
+  */
+
+void quickSort(int *array, int left, int right)
+{
+	int j;
+
+	if (left < right)
+	{
+		j = partition(array, left, right);
+		quickSort(array, left, j);
+		quickSort(array, j + 1, right);
+	}
+}
